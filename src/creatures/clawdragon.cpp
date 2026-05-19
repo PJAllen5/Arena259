81af102c8280c27449de73af6814573aed0fe8d3
Tristan Skerritt <tds@Tristans-MacBook-Pro.local>
2026-05-18 17:51:26 -0700

    Tristan's final lab5 commit

--- src/creatures/clawdragon.cpp
+++ src/creatures/clawdragon.cpp
@@ -9,7 +9,6 @@ Clawdragon::Clawdragon()
     : Creature("Clawdragon", 101, 18){
     }
 
-    
 void Clawdragon::specialMove(Creature& other){
     int slashDamage = RNG::randomValue(10, 25);
     std::cout << getName() << " slashes " << other.getName() << " with claws for "
